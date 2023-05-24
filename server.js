968e9b63db7528094be720b0919c9e86a2f57fb2
nadeeraka <nadeerakan@gmail.com>
2023-05-24 13:48:05 +0530

    init server

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-const express = require("express");
-const app = express();
-const port = process.env.Port || 3500;
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 3500
+const router = require('./router/root')
 
-app.listen(port, () => console.log(`app start at port-${port}`));
+app.use('/', express.static(path.join(__dirname, '/public')))
+
+app.use('/', router)
+
+// app.all('*', (req, res) => {
+//     res.status(404)
+//     if (req.accepts('html')) {
+//         res.sendFile(path.join(__dirname, 'views', '404.html'))
+//     } else if (req.accepts('json')) {
+//         res.json({ message: '404 Not Found' })
+//     } else {
+//         res.type('txt').send('404 Not Found')
+//     }
+// })
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
