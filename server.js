f3a0f6783dc8a1f55e103a0edffd4d8692824a68
nadeeraka <nadeerakan@gmail.com>
2023-05-24 14:15:06 +0530

    log add

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const path = require('path')
 const PORT = process.env.PORT || 3500
 const router = require('./router/root')
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', router)
 
-// app.all('*', (req, res) => {
-//     res.status(404)
-//     if (req.accepts('html')) {
-//         res.sendFile(path.join(__dirname, 'views', '404.html'))
-//     } else if (req.accepts('json')) {
-//         res.json({ message: '404 Not Found' })
-//     } else {
-//         res.type('txt').send('404 Not Found')
-//     }
-// })
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
